a0e691aa8b53f8fb930c2817737f29ad03bd5bca
jamesbigelow <jamesbigelow@gmail.com>
2022-03-05 20:27:05 -0800

    added get_cochleagram and helper functions

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Toolbox for generating, modifying, and analyzing audio data.
 
 See audiotools_demo for example usage.
+
+Last updated 3/5/2022
--- audiotools.py
+++ audiotools.py
@@ -22,7 +22,7 @@ def audio_attenuate( y, atten ):
 
     '''
     
-    Atenuate (audio waveform) 
+    Attenuate (audio waveform) 
     
     INPUT -------
     y : audio signal (sound pressure waveform)
@@ -88,6 +88,281 @@ def audio_write_wav( y, fs, fid ):
     scipy.io.wavfile.write( fid, int(fs), y.astype(np.float32) ) # scipy does not rescale audio data, assumes f32 format
     
     
+
+def filt_convfft( x, y, delay, n=False, opt_truncate=True ):
+    
+    '''
+    
+    Discrete Convolution performed by using FFT 
+    
+    INPUT -------
+    x : signal 1
+    y : signal 2
+    delay : filter delay correction
+    n = fft length (default if not provided)
+    opt_truncate : truncate to x.size + y.size -1 (true/false)
+    
+    RETURN -------
+    f : convolution 
+    
+    '''
+    
+    if not n:
+        n = int( 2 ** ( np.ceil( max( ( np.log10(x.size)/np.log10(2), np.log10(y.size)/np.log10(2) ) ) ) ) )
+        opt_truncate = False
+
+    sx = np.zeros( n*2, dtype=float )
+    sy = np.zeros( n*2, dtype=float )
+    sx[0:x.size] = x
+    sy[0:y.size] = y
+    
+    ftmp = np.real( np.fft.ifft( np.fft.fft(sy) * np.fft.fft(sx) ) )
+    f = ftmp[0+delay:n+delay]
+    
+    if opt_truncate:
+        f = f[ 0: x.size + y.size - 1 ]
+
+    return f    
+    
+
+def filt_bandpass( fs, f1, f2, tw, atten, opt_plot=False ):
+
+    '''
+    
+    Band pass filter
+    
+    INPUT -------
+    fs : audio sample rate, e.g., 48e3
+    f1 : low frequency cutoff (Hz)
+    f2 : high frequency cutoff (Hz)
+    tw : transition width (Hz)
+    atten : pass attenuation 
+    opt_plot : optional plot of filtered power spectrum (true/false)
+    
+    RETURN -------
+    h : impulse response 
+    
+    Example: 
+        fs = 12500
+        f1 = 85.28622364622576
+        f2 = 121.84678710543922
+        tw = 3.6560563459213458
+        atten = 60
+
+    '''
+    
+    
+    if f1 == 0: 
+        h = filt_lowpass( fs, f2, tw, atten )
+    else:
+        fc = ( f2 - f1 ) / 2
+        h = filt_lowpass( fs, fc, tw, atten )
+        fc0 = ( f2 - f1 ) / 2 + f1
+        n = ( h.size - 1 ) / 2
+        h = 2 * h * np.cos( 2 * np.pi * fc0 * ( np.arange( -n, n+1) ) / fs )
+    
+    if opt_plot:
+        
+        m = int( 2 ** np.ceil( np.log2( h.size ) ) * 16 )
+        hh = abs( np.fft.fft(h,m) )
+        ps = 20 * np.log10(hh)
+        
+        fig, ax = plt.subplots(2,1)
+        fig.set_size_inches( 5, 7.5 )
+        
+        ax[0].plot( np.arange( 1, m+1 )/m*fs, ps, color='k' )
+        ax[0].set_xlim( ( 0, fs/2 ) )
+        ax[0].set_ylim( ( min( ps[np.isfinite(ps)] ), 0 ) )
+        ax[0].set_xlabel('Frequency (Hz)')
+        ax[0].set_ylabel('Power spectrum (dB)')
+        
+        ax[1].plot( np.arange( 1, m+1 )/m*fs, hh, color='k' )
+        ax[1].set_xlim( ( f1, f2 ) )
+        ax[1].set_ylim( ( 1-10**(-atten/20), 1+10**(-atten/20) ) )
+        ax[1].set_xlabel('Frequency (Hz)')
+        ax[1].set_ylabel('Linear passband amplitude (dB)') 
+    
+    return h
+
+
+def filt_design( fcr, twr, atten ):
+    
+    '''
+    
+    Filter design
+    
+    INPUT -------
+    fcr : frequency cutoff (radians, 0 : pi)
+    twr : transition width (radians, 0 : pi)
+    atten : attenuation (dB)
+    
+    RETURN -------
+    
+    n : filter length 
+    alpha : filter shape parameter
+    p : filter smoothing parameter 
+    
+    Example: 
+        fcr = 0.37699111843077515
+        twr = 0.09424777960769379
+        atten = 60
+
+    '''
+    
+    # Define p ---------
+    if atten < 22:
+        p =  0
+    elif atten > 121:
+        p = 0.5 / ( 1 + ( ( atten - 120 ) / 20 ) ** 5 ) - 2.5 + 0.063 * atten 
+    else:
+        p = 13 / ( 1 + ( 126 / atten ) ** 1.6 ) - 0.7
+        
+    # Define n ---------
+    if atten > 21 and atten < 121:
+        tmp0 = ( 24.3 / ( 1 + ( 149 / atten ) ** 1.6 ) - 0.085 ) / twr * np.pi - 1
+        tmp1 = p * np.pi / fcr / 0.95
+        n = int( np.round( max( ( tmp0, tmp1 ) ) ) )         
+    elif atten > 120 and atten <= 147:
+        tmp0 = ( -7.5e-4 * ( atten - 200.3 ) ** 2 + 14.74 ) / twr * np.pi - 1
+        tmp1 = p * np.pi / fcr / 0.95
+        n = int( np.round( max( ( tmp0, tmp1 ) ) ) )        
+    elif atten > 147:
+        tmp0 = ( 10.87e-5 * ( atten - 245.6 ) ** 2 -3.1 ) / twr * np.pi - 1
+        tmp1 = p * np.pi / fcr / 0.95
+        n = int( np.round( max( ( tmp0, tmp1 ) ) ) ) 
+        
+    alpha = p * np.pi / ( n + 1 ) / fcr   
+
+    return n, alpha, p
+
+
+def filt_gammatone( fs, freq, bw, n ):
+
+    '''
+    
+    Impulse response of gammatone filter
+    
+    INPUT -------
+    fs : audio sample rate, e.g., 48e3
+    freq : filter characteristic frequency
+    bw : filter bandwidth
+    n : filter order
+    
+    RETURN -------
+    
+    h : impulse response 
+    
+    Example: 
+        fs = 12500
+        freq = 1000
+        n = 3
+        bw = 36.5605
+
+    Reference:
+        Van Immerseel, L., & Peeters, S. (2003). Digital implementation of linear gammatone filters: Comparison of design methods. Acoustics Research Letters Online, 4(3), 59-64.
+    
+    '''
+    
+    # Impulse response
+    p = 0 # phase
+    tvec = np.arange( 0, ( 0.05 * fs ) + 0.05 ) / fs
+    h = tvec ** (n-1) * np.exp( -2 * np.pi * bw * tvec ) * np.cos( 2 * np.pi * freq * tvec + p )
+    
+    # Normalize for unity maximum gain
+    h = h / max( abs( np.fft.fft( h, 1024*64 ) ) )
+    
+    return h
+
+
+def filt_impulse_response( fcr, n, alpha, p ):
+
+    '''
+    
+    Low pass filter
+    
+    INPUT -------
+    fcr : frequency cutoff (radians, 0 : pi)
+    n : filter length 
+    alpha : filter shape parameter
+    p : filter smoothing parameter 
+    
+    RETURN -------
+    h : impulse response 
+    
+    Example: 
+        fcr = 0.37699111843077515
+        n = 148
+        alpha = 0.13082296272948224
+        p = 2.3391145736031422
+
+    '''
+    
+    n_axis = np.arange( -n, n+1 )
+    
+    h = fcr / np.pi * np.sinc( 1 / np.pi * fcr * n_axis )
+    w = np.sinc( 1 / np.pi * alpha * fcr * n_axis / p ) ** p
+    h = h * w
+    
+    return h 
+    
+    
+def filt_lowpass( fs, fc, tw, atten, opt_plot=False ):
+
+    '''
+    
+    Low pass filter
+    
+    INPUT -------
+    fs : audio sample rate, e.g., 48e3
+    fc : frequency cutoff (Hz)
+    tw : transition width (Hz)
+    atten : pass attenuation 
+    opt_plot : optional plot of filtered power spectrum (true/false)
+    
+    RETURN -------
+    h : impulse response 
+    
+    Example: 
+        fs = 12500
+        fc = 750
+        tw = 187.5
+        atten = 60
+
+    '''
+    
+    # cutoff frequency and transition witdh in discrete domain 
+    fcr = 2 * np.pi * fc / fs 
+    twr = 2 * np.pi * tw / fs 
+    
+    # filter params and window
+    n, alpha, p = filt_design( fcr, twr, atten )
+    h = filt_impulse_response( fcr, n, alpha, p )
+    
+    if opt_plot:
+        
+        m = int( 2 ** np.ceil( np.log2( h.size ) ) * 16 )
+        l = int( np.round( m * fc / fs ) )
+        hh = abs( np.fft.fft(h,m) )
+        ps = 20 * np.log10(hh)
+        
+        fig, ax = plt.subplots(2,1)
+        fig.set_size_inches( 5, 7.5 )
+        
+        ax[0].plot( np.arange( 1, m+1 )/m*fs, ps, color='k' )
+        ax[0].set_xlim( ( 0, fs/2 ) )
+        ax[0].set_ylim( ( min( ps[np.isfinite(ps)] ), 0 ) )
+        ax[0].set_xlabel('Frequency (Hz)')
+        ax[0].set_ylabel('Power spectrum (dB)')
+        
+        ax[1].plot( np.arange( 1, l+1 )/m*fs, hh[1:l+1], color='k' )
+        ax[1].set_xlim( ( 0, l/m*fs ) )
+        ax[1].set_ylim( ( 1-10**(-atten/20), 1+10**(-atten/20) ) )
+        ax[1].set_xlabel('Frequency (Hz)')
+        ax[1].set_ylabel('Linear passband amplitude (dB)') 
+
+    return h
+    
+
 def gen_am_sweep_chord( fs, dur, f1, f2, n_carriers, mod_depth, sweep_direction=1 ):
     
     '''
@@ -299,7 +574,7 @@ def gen_click_train( fs, dur, rate ):
     
     return y
 
-
+    
 def gen_dynamic_random_chord( fs, dur, f1, f2, pip_dur, pip_atten, pip_density, opt_plot=False ):
     
     '''
@@ -979,6 +1254,233 @@ def gen_white_noise( fs, dur ):
     y = ( np.random.rand( int( np.round(dur * fs) ) ) * 2 ) - 1
     
     return y
+
+
+def get_cochleagram( y, fs, f1, f2, dx, fm_max, of, atten=60, opt_norm='amp', opt_filt='gamma', opt_bw='erb', opt_plot=False ):
+
+    '''
+
+    Cochelagram model of audio waveform.
+    
+    INPUT -------
+    y : audio signal (sound pressure waveform)
+    fs : audio sample rate (Hz), e.g., 48e3
+    f1 : low frequency (Hz)
+    f2 : high frequency (Hz)
+    dx : spectral separation betwen adjacent filters in octaves, e.g., 1/8
+    fm_max : frequency-modulation max allowed for temporal envelope at each band. In inf, full range is used.
+    of: oversampling factor for temporal envelope. Since the max frequency of the envelope is fm_max, the frequency used to sample the envelope is 2*fm_max*of
+    atten : attenuation error for modulation low pass filter, dB
+    opt_norm : type of normalization, options == 'amp' for equal amplitude, 'en' for equal energy
+    opt_filt : type of filter, options == 'gamma', 'bspline'
+    opt_bw : type of bandwidth, options == 'erb' for equivalent rectangular bandwidth, 'cb' for critical bandwidth 
+    opt_plot : optional cochleagram plot (true/false)
+
+    RETURN -------
+    c : cochleagram 
+    cc : cochleagram corrected for group delay 
+    c_db : cochleagram in dB units
+    cc_db : cochleagram corrected for group delay in dB units
+    axis_time : time axis for cochleagram (s)
+    axis_freq : frequency axis for stim matrix (Hz)
+      
+    Example: 
+        f1 = 100
+        f2 = fs/2
+        dx = 0.1
+        fm_max = 750
+        of = 2
+        atten = 60
+        opt_norm ='amp'
+        opt_filt = 'gamma'
+        opt_bw = 'erb'    
+        
+    Attribution: 
+        This function is a revised implementation of MATLAB functions provided by:
+        Khatami, F., & Escabí, M. A. (2020). Spiking network optimized for word recognition in noise predicts auditory system hierarchy. PLoS computational biology, 16(6), e1007558.
+    
+    '''
+    
+    # hard coded params - - - - 
+    n = 3 # filter order
+    delay = 0
+    opt_truncate = True
+        
+    # Frequency axis for chromatically-spaced filter bank (f[k] = f[k+1]*2**dx)
+    xn = np.log2( f2/f1 )
+    nc = np.floor( xn/dx )
+    xc = np.arange( 0.5, nc+0.5 ) / nc * xn
+    axis_freq = f1 * 2 ** xc
+    
+    if opt_bw == 'erb':
+        bw = get_erb( axis_freq )
+    elif opt_bw == 'cb':
+        bw = get_critical_bw( axis_freq )
+    
+    # Temporal downsampling factor 
+    df = int( max( ( 1, np.ceil( fs / 2 / fm_max / of ) ) ) )
+    dfi = np.arange( 0, y.size, df ) # indices
+    
+    # Low pass filter for extracting envelope 
+    fc = fm_max
+    tw = fc * 0.25
+    h_lo = filt_lowpass( fs, fc, tw, atten )
+    n_lo = int( ( h_lo.size - 1 ) / 2 )
+    
+    # Generate filters 
+    if opt_filt == 'gamma':
+        htmp = filt_gammatone( fs, axis_freq[0], bw[0], n )
+        filters = np.zeros( ( axis_freq.size, htmp.size ), dtype=float )
+        filter_n = np.zeros( axis_freq.size, dtype=float )
+        for ii in range(axis_freq.size):
+            filters[ii,:] = filt_gammatone( fs, axis_freq[ii], bw[ii], n )
+            filter_n[ii] = ( filters[ii,:].size - 1 ) / 2   
+            
+    # Find group delays 
+    group_delay = np.zeros( axis_freq.size, dtype=float )
+    for ii in range(axis_freq.size):
+        p = filters[ii,:] ** 2 / np.sum( filters[ii,:] ** 2 )
+        t = np.arange( 1, filters[ii,:].size+1 ) / fs
+        group_delay[ii] = np.sum( p * t )
+    nmax = round( max( group_delay * fs ) )
+    ndelay = round( group_delay[0] * fs ) +1
+    dfic = np.arange( ndelay, y.size - nmax + ndelay -1 +df, df ) # delay corrected downsampled indices
+    
+    # Filter signal, extract envelope, downsample, store cochleagram model
+    nfft = int( 2 ** nextpow2( y.size + max(filter_n)*2+1 + n_lo*2+1 ) ) # FFT size
+    c = np.zeros( ( axis_freq.size, dfi.size ), dtype=float )
+    cc = np.zeros( ( axis_freq.size, dfic.size ), dtype=float )
+    for ii in range(axis_freq.size):
+        
+        # Filter (gammatone or bspline)
+        h = filters[ii,:]
+        if opt_norm == 'en':
+            h = h / np.sqrt( np.sum( h ** 2 ) ) # equal energy 
+        Y = filt_convfft( y, h, delay, nfft, opt_truncate )
+        
+        # Low pass filter 
+        Y = abs( signal.hilbert( Y ) ) # Get envelope via Hilbert transform
+        Y = filt_convfft( Y, h_lo, n_lo )
+        Y[Y<0] = 0 # remove negative values due to filtering
+        
+        # store cochleagram frequency band
+        c[ii,:] = Y[dfi] # downsample
+    
+        # ...(delay corrected)  
+        ndelay = round( group_delay[0] * fs ) +1
+        cc[ii,:] = Y[ dfic ] # downsample
+    
+    # dB models - - - - - 
+    c_db = 20 * np.log10(c)
+    idx = np.isfinite( c_db )
+    mn = min( c_db[idx] )
+    idx = np.isinf( c_db )
+    c_db[idx] = mn # replace -inf values with min
+    c_db -= np.mean( c_db ) # mean subtraction 
+    
+    cc_db = 20 * np.log10(cc)
+    idx = np.isfinite( cc_db )
+    mn = min( cc_db[idx] )
+    idx = np.isinf( cc_db )
+    cc_db[idx] = mn # replace -inf values with min
+    cc_db -= np.mean( cc_db ) # mean subtraction 
+    
+    axis_time = np.arange( 0, c.shape[1] ) / (fs/df)
+    
+    if opt_plot:
+
+        plt.close('all')
+        fig, axs = plt.subplots(2, 2)
+        fig.set_size_inches((18,12))
+                
+        axs[0,0].imshow( c, cmap='gray_r', origin='lower', aspect='auto', extent=[ min(axis_time), max(axis_time), min(axis_freq), max(axis_freq) ] )
+        axs[0,0].set_xlabel('Time (s)')
+        axs[0,0].set_ylabel('Frequency (Hz)')
+        axs[0,0].set_title('Cochleagram')
+        
+        axs[0,1].imshow( cc, cmap='gray_r', origin='lower', aspect='auto', extent=[ min(axis_time), max(axis_time), min(axis_freq), max(axis_freq) ] )
+        axs[0,1].set_xlabel('Time (s)')
+        axs[0,1].set_ylabel('Frequency (Hz)')
+        axs[0,1].set_title('Cochleagram - delay corrected')
+        
+        axs[1,0].imshow( c_db, cmap='gray_r', origin='lower', aspect='auto', extent=[ min(axis_time), max(axis_time), min(axis_freq), max(axis_freq) ] )
+        axs[1,0].set_xlabel('Time (s)')
+        axs[1,0].set_ylabel('Frequency (Hz)')
+        axs[1,0].set_title('Cochleagram dB')
+        
+        axs[1,1].imshow( cc_db, cmap='gray_r', origin='lower', aspect='auto', extent=[ min(axis_time), max(axis_time), min(axis_freq), max(axis_freq) ] )        
+        axs[1,1].set_xlabel('Time (s)')
+        axs[1,1].set_ylabel('Frequency (Hz)')
+        axs[1,1].set_title('Cochleagram dB - delay corrected')        
+
+    return c, cc, c_db, cc_db, axis_time, axis_freq
+
+
+def get_critical_bw( f ):
+    
+    '''
+    
+    Calc critical bandwidth  
+    
+    INPUT -------
+    f : frequency (Hz)
+
+    RETURN -------
+    b :  critical bandwidth
+    
+    Example: 
+        f = 1e3
+        
+    '''
+    
+    b = 94 + 71 * ( f / 1000 ) ** (3/2)
+        
+    return b
+    
+
+def get_erb( f ):
+    
+    '''
+    
+    Calc equivalent rectangular bandwidth  
+    
+    INPUT -------
+    f : frequency (Hz)
+
+    RETURN -------
+    b :  equivalent rectangular bandwidth
+    
+    Example: 
+        f = 1e3
+        
+    '''
+    
+    b = 1.019 * 24.7 * ( 1 + 4.37 * f / 1000 )
+        
+    return b
+
+
+def nextpow2( x ):
+    
+    '''
+    
+    Next higher power of 2, the first p such that 2.^p >= abs(x)
+
+    INPUT -------
+    x : integer
+
+    RETURN -------
+    n : power
+    
+    '''
+    
+    p = 2
+    while p < x: 
+        p = p * 2
+    p = np.log2( p )
+    
+    
+    return p
 
 
 def plot_spectrogram( y, fs ):
